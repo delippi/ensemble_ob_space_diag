ff3bbe544ebd291616d12da2dfb20768e9848063
Donald.E.Lippi <donald.e.lippi@noaa.gov>
2023-03-08 19:05:32 +0000

    initial commit for jet branch

--- ensemble_diags_profile.py
+++ ensemble_diags_profile.py
@@ -26,7 +26,7 @@ expt_names = []
 #                        USER SPECIFIED PARAMETERS                  *
 # ********************************************************************
 n_mem = 30
-expt_names.append("rrfs_a_conus")
+expt_names.append("RRFS_CONUS")
 # expt_names.append("rrfs_a_na")
 # expt_names.append("just uncomment for a second experiment")
 
--- rt_ensemble_diags_profile.sh
+++ rt_ensemble_diags_profile.sh
@@ -1,39 +1,15 @@
 #!/bin/bash
 
-debug="NO"
+py=/home/Donald.E.Lippi/miniconda3/bin/python
 
-py=/u/donald.e.lippi/miniconda3/bin/python
+export incdate=/home/Donald.E.Lippi/bin/incdate
 
-export incdate=/u/donald.e.lippi/bin/incdate
+outdir="./"
+#/mnt/lfs1/BMC/wrfruc/chunhua/May2022_retro/Ens_Ctrl/NCO_dirs/nwges/para/RRFS_CONUS/observer_diag
+datapath="/mnt/lfs1/BMC/wrfruc/chunhua/May2022_retro/Ens_Ctrl/NCO_dirs/nwges/para/"  # RRFS_CONUS/observer_diag
 
-datapath="/lfs/h2/emc/ptmp/donald.e.lippi/rrfs_a_diags/"
-
-runtime=$(date --date "2 hour ago" "+%Y%m%d%H")
-runtime=$(date --date "24 hour ago" "+%Y%m%d%H")
-
-
-year=`echo $runtime | cut -c 1-4`
-mon=`echo $runtime | cut -c 5-6`
-day=`echo $runtime | cut -c 7-8`
-
-#year=2023
-#mon=03
-#day=03
-cyc=23
-
-runtime=${year}${mon}${day}${cyc}
 hours=23
-#hours=17
-
-if [[ $debug == "YES" ]]; then
-    #run manually
-    runtime=$(date --date "now" "+%Y%m%d"00)
-    runtime=2023021412
-    hours=12
-fi
-
-
-date2=$runtime
+date2=2022051223
 date1=`$incdate $date2 -$hours`
 echo $date1 $date2
 
@@ -41,20 +17,8 @@ year=`echo $date2 | cut -c 1-4`
 mon=`echo $date2 | cut -c 5-6`
 day=`echo $date2 | cut -c 7-8`
 
-
-if [[ $debug == "YES" ]]; then
-    outdir="./"
-else
-    outdir=/lfs/h2/emc/ptmp/donald.e.lippi/rrfs_a_diags/figs/$year/$mon/$day/
-    mkdir -p $outdir
-fi
 cd $outdir
-
-$py /lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/ensemble_ob_space_diag/ensemble_diags_profile.py $date1 $date2 $datapath
-
-rzdm_config_files=/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/config/
-cp ${rzdm_config_files}/* .
+$py /mnt/lfs1/data_untrusted/Donald.E.Lippi/ensemble_ob_space_diag/ensemble_diags_profile.py $date1 $date2 $datapath
 
 echo ""
 echo "your files are located: $outdir"
-
