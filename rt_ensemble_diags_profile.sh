e5ad62a356a52a04ca00fa5d60f255a7990fb13b
donald lippi <donald.e.lippi@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2023-03-16 18:22:14 +0000

    update main bash driver scripts.

--- rt_ensemble_diags_profile.sh
+++ rt_ensemble_diags_profile.sh
@@ -1,59 +1,41 @@
 #!/bin/bash
-
-debug="NO"
-
-py=/u/donald.e.lippi/miniconda3/bin/python
-
-export incdate=/u/donald.e.lippi/bin/incdate
-
-datapath="/lfs/h2/emc/ptmp/donald.e.lippi/rrfs_a_diags/"
-
-runtime=$(date --date "2 hour ago" "+%Y%m%d%H")
-runtime=$(date --date "24 hour ago" "+%Y%m%d%H")
-
-
-year=`echo $runtime | cut -c 1-4`
-mon=`echo $runtime | cut -c 5-6`
-day=`echo $runtime | cut -c 7-8`
-
-#year=2023
-#mon=03
-#day=03
-cyc=23
-
-runtime=${year}${mon}${day}${cyc}
-hours=23
-#hours=17
-
-if [[ $debug == "YES" ]]; then
-    #run manually
-    runtime=$(date --date "now" "+%Y%m%d"00)
-    runtime=2023021412
-    hours=12
+machine=`hostname | cut -c 1`
+if [[ $machine == "d" || $machine == "c" ]]; then
+    py=/u/donald.e.lippi/miniconda3/bin/python
+    export incdate=/u/donald.e.lippi/bin/incdate
 fi
 
 
-date2=$runtime
+############### USER INPUT ##################
+date2=2023031523
+hours=23
+expt="rrfs_a_conus"
+datapath_rt="/lfs/h2/emc/ptmp/emc.lam/rrfs/v0.3.8/nwges/observer_diag/" #2023031612
+datapath="/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/ensemble_ob_space_diag/" # rrfs_a_conus
+figdir="/lfs/h2/emc/ptmp/donald.e.lippi/rrfs_a_diags/figs/"
+scriptpath="/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/ensemble_ob_space_diag/"
+############END USER INPUT ##################
+
 date1=`$incdate $date2 -$hours`
 echo $date1 $date2
-
 year=`echo $date2 | cut -c 1-4`
 mon=`echo $date2 | cut -c 5-6`
 day=`echo $date2 | cut -c 7-8`
 
-
-if [[ $debug == "YES" ]]; then
-    outdir="./"
-else
-    outdir=/lfs/h2/emc/ptmp/donald.e.lippi/rrfs_a_diags/figs/$year/$mon/$day/
-    mkdir -p $outdir
+if [[ ! -L $expt ]]; then
+    ln -sf $datapath_rt ./$expt
 fi
+
+outdir="$figdir/$year/$mon/$day/"
+mkdir -p $outdir
 cd $outdir
+$py $scriptpath/ensemble_diags_profile.py $date1 $date2 $datapath
 
-$py /lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/ensemble_ob_space_diag/ensemble_diags_profile.py $date1 $date2 $datapath
-
-rzdm_config_files=/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/config/
-cp ${rzdm_config_files}/* .
+# Only copy rzdm files when on WCOSS.
+if [[ $machine == "d" || $machine == "c" ]]; then
+    rzdm_config_files=/lfs/h2/emc/da/noscrub/donald.e.lippi/rrfs_mon/config/
+    cp ${rzdm_config_files}/* .
+fi
 
 echo ""
 echo "your files are located: $outdir"
